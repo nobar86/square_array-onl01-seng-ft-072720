180bdcffb2494d2ba3ba6da69a185dbc78a82abf
Michael Rabon <nobar86@gmail.com>
2020-08-03 01:39:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,9 @@
-def square_array(array)
-  squared_array = []
- array.each do |num|
-  squared_array <<  num * num
+# def square_array(array)
+#   squared_array = []
+# array.each do |num|
+#   squared_array <<  num * num
    
-end
-squared_array
-end
+# end
+# squared_array
+# end
 
